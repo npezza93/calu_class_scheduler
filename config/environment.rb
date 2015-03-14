aa3030699657186272aac85824d3a5a161948cc3
npezza93 <pez1900@calu.edu>
2015-03-14 19:43:17 +0000

    mailer edits

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :domain => 'https://calu-advisor.herokuapp.com/',
+    :authentication => :plain,
+    :user_name => 'beautiful.soup.notification',
+    :password => 'CalU011293!!15'
+}
