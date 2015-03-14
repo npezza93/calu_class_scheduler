810cc4315d21b9914badc80058caea4f97e05c13
npezza93 <pez1900@calu.edu>
2015-03-14 19:49:58 +0000

    mailer updates

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.smtp_settings = {
-    :enable_starttls_auto => true,
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :domain => 'https://calu-advisor.herokuapp.com/',
-    :authentication => :plain,
-    :user_name => 'beautiful.soup.notification',
-    :password => 'CalU011293!!15'
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
 }
