9e5c8e6a1cf9e765a22637f73aca2884af881cdf
npezza93 <pez1900@calu.edu>
2015-03-14 20:02:42 +0000

    mailer updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,13 +75,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'calu-advisor.herokuappp.com',
-  :enable_starttls_auto => true
-}
+  config.action_mailer.default_url_options = { :host => 'calu-advisor.herokuapp.com' }
 end
