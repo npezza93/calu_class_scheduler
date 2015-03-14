4caf9aefbe806d18b0517df541297a17f1fa7760
npezza93 <pez1900@calu.edu>
2015-03-14 20:40:14 +0000

    mailer updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
     :address        => 'smtp.gmail.com',
     :port           => 587,
     :authentication => :plain,
-    :user_name      => ENV['GMAIL_USERNAME'],
-    :password       => ENV['GMAIL_PASSWORD'],
+    :user_name => 'beautiful.soup.notification',
+    :password => 'CalU011293!!15',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
