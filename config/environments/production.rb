1cd81cd6eea0b832d894e698d5309a19ea1a8032
npezza93 <pez1900@calu.edu>
2015-03-15 00:22:22 +0000

    offering import update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  config.assets.compile = true
+config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
   # Generate digests for assets URLs.
   config.assets.digest = true
 
@@ -87,4 +87,5 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
+
 end
