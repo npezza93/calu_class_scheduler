8ed28bfab1c0f5d23911c64a4417289acbe1e1af
npezza93 <npezza93@gmail.com>
2016-05-21 14:50:48 -0400

    Remove duplication in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,27 +1,34 @@
 class UserMailer < ActionMailer::Base
   def submit_to_advisor(approval)
-    @approval = approval
-    @user = approval.user
-    @schedules = @user.courses
+    set_users_schedules(approval)
+
     mail to: @user.advisor_prof.email,
          subject: 'Schedule Submitted for Your Approval'
   end
 
   def student_confirmation(approval)
-    @user = approval.user
-    @schedules = @user.schedules
+    set_users_schedules(approval)
+
     mail to: @user.email, subject: 'Schedule Submitted for Approval'
   end
 
   def advisor_confirmation(approval)
-    @user = approval.user
-    @schedules = @user.schedules
+    set_users_schedules(approval)
+
     mail to: @user.advisor_prof.email, subject: 'Schedule Approval Confirmation'
   end
 
   def approved(approval)
-    @user = approval.user
-    @schedules = @user.schedules
+    set_users_schedules(approval)
+
     mail to: @user.email, subject: 'Your Schedule Has Been Approved!'
+  end
+
+  private
+
+  def set_users_schedules(approval)
+    @approval = approval
+    @user = approval.user
+    @schedules = @user.courses
   end
 end
