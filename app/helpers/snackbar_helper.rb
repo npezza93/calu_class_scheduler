6d4c2eb10009504a0133a1e5781cbd3b72e87913
npezza93 <npezza93@gmail.com>
2017-03-12 12:51:53 -0400

    Add snackbar. WIP refactoring application.html.haml

---
+++ app/helpers/snackbar_helper.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+module SnackbarHelper
+  def snackbar_notice(message)
+    return if message.blank?
+
+    content_tag(:div, snackbar_text(message), class: "mdc-snackbar", aria: {
+      live: :assertive, atomic: "", hidden: ""
+    })
+  end
+
+  def snackbar_text(message)
+    content_tag(:div, message, class: "mdc-snackbar__text") + snackbar_button
+  end
+
+  def snackbar_button
+    content_tag(:div, class: "mdc-snackbar__action-wrapper") do
+      content_tag(
+        :button, nil,
+        class: "mdc-button mdc-snackbar__action-button", type: :button
+      )
+    end.html_safe
+  end
+end
