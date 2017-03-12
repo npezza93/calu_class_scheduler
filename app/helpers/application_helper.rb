5170f72f9f0441443db15b1aa8e7926ddf1648af
npezza93 <npezza93@gmail.com>
2017-03-12 13:06:53 -0400

    Update fab buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,33 +1,13 @@
 # frozen_string_literal: true
 module ApplicationHelper
-  def new_fab(link, fixed = true)
-    button_classes = "#{fab_classes} new-fab #{'fixed' if fixed}"
-    link_to link do
-      content_tag(:button, class: button_classes) do
-        content_tag(:i, "add", class: "material-icons")
+  def fab(link, fixed = true)
+    button_classes =
+      "mdc-fab material-icons new-fab #{'app-fab--absolute' if fixed}"
+
+    link_to link, class: button_classes, "data-mdc-auto-init" => "MDCRipple" do
+      content_tag(:span, class: "mdc-fab__icon") do
+        "add"
       end
     end
-  end
-
-  def edit_fab(link)
-    button_classes = "#{fab_classes} edit-fab fixed"
-    link_to link do
-      content_tag(:button, class: button_classes) do
-        content_tag(:i, "edit", class: "material-icons")
-      end
-    end
-  end
-
-  def destroy_fab(link, confirm_message = nil)
-    confirm_message ||= "Are you sure you want to delete this?"
-    link_to link, data: { method: :delete, confirm: confirm_message } do
-      content_tag(:button, class: "#{fab_classes} destroy-fab fixed") do
-        content_tag(:i, "clear", class: "material-icons")
-      end
-    end
-  end
-
-  def fab_classes
-    "mdl-button mdl-js-button mdl-button--fab"
   end
 end
