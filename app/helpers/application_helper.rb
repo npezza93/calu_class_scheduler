97879a3ba39e5f0c58cc4ab0965c1ea95b763567
npezza93 <npezza93@gmail.com>
2017-03-19 13:59:25 -0400

    Cleanup application.html.haml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,22 @@ module ApplicationHelper
       content_tag(:span, "add", class: "mdc-fab__icon")
     end
   end
+
+  def render_title(title)
+    if title.present?
+      "#{title} | CalU Advisor"
+    else
+      "CalU Advisor"
+    end
+  end
+
+  def render_back_url(url)
+    if url.present?
+      link_to url, class: "white-text layout horizontal mr-3 ml-1" do
+        material_icon.arrow_back.to_s
+      end
+    else
+      content_tag(:i, nil, class: "mr-3 ml-1 material-icons")
+    end
+  end
 end
