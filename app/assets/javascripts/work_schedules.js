24b98dfe826d00b26ab860d174d3f5def60582d3
npezza93 <npezza93@gmail.com>
2017-03-29 18:17:07 -0400

    Fix up work schedules calendar

---
+++ app/assets/javascripts/work_schedules.js
@@ -0,0 +1,33 @@
+$(document).on("turbolinks:load", function() {
+  if ($(".schedule").length > 0) {
+    $("[data-id]").each(function() {
+      expandClassRow($(this));
+    })
+  }
+});
+
+function expandClassRow(element) {
+  var td = element;
+  var tr = element.closest("tr");
+  var id = element.data("id");
+
+  var currentCol = td.index();
+  var nextRow = td.closest("tr").index() + 1;
+  var rowspans = $(".calendar td[rowspan]");
+
+  rowspans.each(function () {
+      var rs = $(this);
+      var rsIndex = rs.closest("tr").index();
+      var rsQuantity = parseInt(rs.attr("rowspan"));
+      if (nextRow > rsIndex && nextRow <= rsIndex + rsQuantity - 1) {
+          currentCol--;
+      }
+  });
+
+  var nextVert = $(".calendar tr").eq(nextRow).children().eq(currentCol);
+
+  if (nextVert.data("id") == id) {
+    td.attr("rowspan", (td.attr("rowspan") || 1) + 1);
+    nextVert.remove();
+  }
+}
